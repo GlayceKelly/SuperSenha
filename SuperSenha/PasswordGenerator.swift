d2048d62ce572ea5a14c7c97371c724e0ff31cd5
Glayce <glayce@Wesleys-MacBook-Air.local>
2020-03-29 20:03:40 -0300

    first commit

---
+++ SuperSenha/PasswordGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  PasswordGenerator.swift
+//  SuperSenha
+//
+//  Created by Glayce on 28/03/20.
+//  Copyright © 2020 Glayce. All rights reserved.
+//
+
+import Foundation
---
+++ SuperSenha/SecurityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecurityViewController.swift
+//  SuperSenha
+//
+//  Created by Glayce on 28/03/20.
+//  Copyright © 2020 Glayce. All rights reserved.
+//
+
+import UIKit
+
+class SecurityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
